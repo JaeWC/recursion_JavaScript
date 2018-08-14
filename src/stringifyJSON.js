e94584c53eca55c961b3cdd7528f421dabd682be
Jae Choi <jaechoi@Jae-MacBook-Pro.local>
2018-08-14 17:36:14 +0900

    stringifyJSON complete

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,39 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  if (typeof obj === 'string') {
+    return '"' + String(obj) + '"';
+  } else if (typeof obj === 'number' || typeof obj === 'boolean' || obj === null) {
+    return String(obj); 
+  }
+
+  if (Array.isArray(obj)) {
+    if (obj.length === 0) {
+      return '[]';
+    } else {
+      var resultArray = '[' + stringifyJSON(obj[0]);
+      for (var i = 1; i < obj.length; i++) {
+        resultArray += ',';
+        resultArray += stringifyJSON(obj[i]);
+      }
+      resultArray += ']';
+      return resultArray;
+    }
+  } else {
+      if (Object.keys(obj).length === 0) {
+        return '{}';
+      } else {
+        var resultObject = '{';
+        for (var prop in obj) {
+          if (obj[prop] === undefined || typeof obj[prop] === 'function' || obj[prop === null]) {
+            delete obj[prop];
+            return stringifyJSON(obj);
+          } else {
+            resultObject += stringifyJSON(prop) + ":" + stringifyJSON(obj[prop]) + ',';
+          }
+        }
+        return resultObject.substr(0, resultObject.length - 1) + '}';
+      }
+    }
+
 };
